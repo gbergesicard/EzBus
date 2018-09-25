f0ddcbb9802ed37994c048d0ece4af354ed36a33
gleclere <guillaume.leclere@sage.com>
2018-09-25 18:59:37 +0200

    Merge hardware management

--- EzBus.ino
+++ EzBus.ino
@@ -1,19 +1,33 @@
-#include <PubSubClient.h>
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h> 
-#include <ESP8266WebServer.h>
-#include <EEPROM.h>
-#include <string>
-#include <DNSServer.h>
-#include "ArduinoJson.h"
-#include <FS.h> // file system management
+#include <string>               // Strings
+#include <ESP8266WiFi.h>        // WiFi driver
+#include <ESP8266WebServer.h>   // WebServer management
+#include <DNSServer.h>          // DNS management
+#include "ArduinoJson.h"        // JSON parser
+#include <FS.h>                 // File system management
+#include <SPI.h>                // SPI management for RFID reader
+#include <MFRC522.h>            // RFID reader management
+#include <LiquidCrystal_I2C.h>  // LCD screen management
+#include <Ticker.h>             // LCD backlight timeout management
 
 #define REV "REV0001"
+
+// LED pins
+#define LED_PIN_RED   0 
+#define LED_PIN_GREEN 2
+
+// MFRC522 pins
+#define RST_PIN 16 // D0
+#define SS_PIN 15 // D8
+
+#define BACKLIGHT_TIMER 5 // 5 seconds
 
 int debug = 1;
 File fsUploadFile;              // a File object to temporarily store the received file
 ESP8266WebServer server(80);    // Web server on port 80 
 WiFiClient client;              // wifi client 
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+LiquidCrystal_I2C lcd(0x27, 16, 2); // Set the LCD address to 0x27 for a 16 chars and 2 line display
+Ticker backlightTimer;
 IPAddress apIP(192, 168, 1, 1);
 // for the captive network
 const byte DNS_PORT = 53;
@@ -67,54 +81,111 @@ void UploadPage(){
   server.send( 200 , "text/html", s);
 }
 
+/*
+ * Callback for backlight saver management
+ */
+void callbackBacklight() {
+  lcd.noBacklight();
+  backlightTimer.detach();
+}
+
 void setup() {
-    WiFi.mode(WIFI_AP);
-    WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
-    WiFi.softAP("Wifi device setup");
-    delay(100); //Stable AP
+  WiFi.mode(WIFI_AP);
+  WiFi.softAPConfig(apIP, apIP, IPAddress(255, 255, 255, 0));
+  WiFi.softAP("Wifi device setup");
+  delay(100); //Stable AP
+  if(debug == 1){
+    Serial.begin(115200); //Set Baud Rate
+    Serial.print("IP address:\t");
+    Serial.println(WiFi.localIP());           // Send the IP address of the ESP8266 to the computer
+  }
+
+  // Setting LED pins
+  pinMode(LED_PIN_RED, OUTPUT);
+  pinMode(LED_PIN_GREEN, OUTPUT);
+
+  // Init the LCD
+  lcd.begin();
+
+  // Init SPI bus
+  SPI.begin();
+
+  // Init MFRC522 card
+  mfrc522.PCD_Init();
+
+  // initilize file system
+  SPIFFS.begin();
+
+  if (isFileExists(jsonFileName)){
+    UpdateJson(jsonFileName,&root);
     if(debug == 1){
-      Serial.begin(115200); //Set Baud Rate
-      Serial.print("IP address:\t");
-      Serial.println(WiFi.localIP());           // Send the IP address of the ESP8266 to the computer
+      char chTempo[30];
+      chTempo[0] ='\0';
+      
+      sprintf(chTempo,"Setup pointeur root :%Ox",root);
+      traceChln(chTempo);
     }
+  }
+  dnsServer.start(DNS_PORT, "*", apIP);
+  server.onNotFound(D_AP_SER_Page);
+  server.on("/",D_AP_SER_Page);
+  server.on("/upload", HTTP_POST,                       // if the client posts to the upload page
+    [](){ server.send(200); },                          // Send status 200 (OK) to tell the client we are ready to receive
+    handleFileUpload                                    // Receive and save the file
+  );
+  server.on("/upload", HTTP_GET, []() {                 // if the client requests the upload page
+  if (!handleFileRead("/EZBus.json"))                // send it if it exists
+    server.send(404, "text/plain", "404: Not Found"); // otherwise, respond with a 404 (Not Found) error
+  });
+  server.on("/up",UploadPage);
+  
+  server.begin();
+  // captive network is active 
+  captiveNetwork = 1;
 
-    // initilize file system
-    SPIFFS.begin();
-
-    if (isFileExists(jsonFileName)){
-      UpdateJson(jsonFileName,&root);
-      if(debug == 1){
-        char chTempo[30];
-        chTempo[0] ='\0';
-        
-        sprintf(chTempo,"Setup pointeur root :%Ox",root);
-        traceChln(chTempo);
-      }
-    }
-    dnsServer.start(DNS_PORT, "*", apIP);
-    server.onNotFound(D_AP_SER_Page);
-    server.on("/",D_AP_SER_Page);
-    server.on("/upload", HTTP_POST,                       // if the client posts to the upload page
-      [](){ server.send(200); },                          // Send status 200 (OK) to tell the client we are ready to receive
-      handleFileUpload                                    // Receive and save the file
-    );
-    server.on("/upload", HTTP_GET, []() {                 // if the client requests the upload page
-    if (!handleFileRead("/EZBus.json"))                // send it if it exists
-      server.send(404, "text/plain", "404: Not Found"); // otherwise, respond with a 404 (Not Found) error
-    });
-    server.on("/up",UploadPage);
-    
-    server.begin();
-    // captive network is active 
-    captiveNetwork = 1;
+  // Setup end
+  lcdPrint(0,0,"EZBus ready!");
+  lcdPrint(1,0,REV);
 }
 
 void loop() {
-
+  digitalWrite(LED_PIN_RED, LOW);
+  digitalWrite(LED_PIN_GREEN, LOW);
+  
   if (captiveNetwork == 1){
     dnsServer.processNextRequest();
   }
   server.handleClient();
+
+  // Look for new cards
+  if (mfrc522.PICC_IsNewCardPresent()) {
+    digitalWrite(LED_PIN_RED, HIGH);
+    
+    // Select one of the cards
+    if ( mfrc522.PICC_ReadCardSerial()) {
+      clearLine(1);
+      String tagId = getStringFromByteArray(mfrc522.uid.uidByte, mfrc522.uid.size);
+
+      if (debug==1) { Serial.println("Tag Id: " + tagId); };
+
+      // TO DO: search the TagId in JSON
+      /*if (existTag(tagId)) {
+          digitalWrite(LED_PIN_RED, LOW);
+          digitalWrite(LED_PIN_GREEN, HIGH);
+          wait(1000);
+        }
+      }*/
+    }
+  }
+}
+
+String getStringFromByteArray(byte *buffer, byte bufferSize) {
+  String myString = "";
+  for (byte i = 0; i < bufferSize; i++) {
+      myString += (buffer[i] < 0x10 ? "0" : "");
+      myString += String(buffer[i], HEX);
+  }
+  return myString;
 }
 
 String getContentType(String filename) { // convert the file extension to the MIME type
@@ -216,4 +287,23 @@ void traceCh(char* chTrace){
   if (debug == 1){
     Serial.print(chTrace);
   }
+}
+
+// LCD management
+void lcdPrint(int line, int col, String text) {
+  lcd.setCursor(col,line);
+  lcd.print(text);
+  lcd.backlight();
+
+  backlightTimer.attach(BACKLIGHT_TIMER, callbackBacklight);
+}
+
+void lcdPrintPassager(String passenger){
+  clearLine(1);
+  lcdPrint(1,0,passenger);
+}
+
+void clearLine(int ligne){
+  lcd.setCursor(0,ligne);
+  lcd.print("                    ");
 }
